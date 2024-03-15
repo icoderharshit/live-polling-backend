39f07d395be7d2c6fe8bd50269914653c0486cf9
Harshit Sharma <iharshitsharma@outlook.com>
2024-03-15 14:00:34 +0530

    hardcode: PORT

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
