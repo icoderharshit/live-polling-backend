3eaae5bb8356d6ee0d156f95ab9369dbafd72f69
Harshit Sharma <iharshitsharma@outlook.com>
2024-03-15 13:19:55 +0530

    update port

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ const {Server} = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(
-    server, {
-        path:'/socket',
-        reconnection: true,
-        transports:['websocket', 'polling'],
-        cors:{
-            origin:'*'
-        }
-    }
+    server
 )
 
 let activePoll = null;
@@ -67,7 +60,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT =  3001;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
