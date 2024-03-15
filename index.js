e74785344c104bba30a60c50213c5b067200ea21
Harshit Sharma <iharshitsharma@outlook.com>
2024-03-15 13:06:33 +0530

    server update

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const {Server} = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(
+    server, {
+        path:'/socket',
+        cors:{
+            origin:'*'
+        }
+    }
+)
 
 let activePoll = null;
 let studentAnswers = {};
